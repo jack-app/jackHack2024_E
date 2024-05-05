e284e7ca481cb139faf7a23b9c20b65cf35f897d
teteen <jiatengren92@gmail.com>
2024-05-05 12:09:06 +0900

    startpage kansei

--- kikikan/src/pages/Explain/explain.jsx
+++ kikikan/src/pages/Explain/explain.jsx
@@ -1,9 +1,12 @@
 import {NavLink} from "react-router-dom";
+import { StartButton } from "../../components/StartButton/startButton";
 
 export const Explain = () => {
   return (
     <div>
       <h1>Explain</h1>
+      <StartButton name="GAME START" />
+
     </div>
   );
 };
--- kikikan/src/pages/Start/start.jsx
+++ kikikan/src/pages/Start/start.jsx
@@ -1,23 +1,33 @@
-import {NavLink} from "react-router-dom";
-import logo from "../../assets/kikikan2 1.png"
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/kikikan2 1.png";
 import city from "../../assets/city1.png";
 import start1 from "../../assets/start.jsx.png";
 import toguro from "../../assets/image 2.png";
 import homeless from "../../assets/image 3.png";
 import George from "../../assets/face.png";
-import './start.css'
+import { StartButton } from "../../components/StartButton/startButton";
+import { ExplainButton } from "../../components/ExplainButton/ExplainButton";
+import "./start.css";
 
 export const Start = () => {
   return (
-    <div className="hogehoge">
-      <img src={logo} className="logo"/>
+    <div className="start-page-context">
+      <img src={logo} alt="" className="logo" />
       {/* <img src={city} className="city"/> */}
       {/* <img src={start} className="start"/> */}
-      <img src={toguro} className="toguro"/>
-      <img src={homeless} className="homeless"/>
-      <img src={George} className="George"/>
+      <img src={toguro} alt="" className="toguro" />
+      <div className="start_homeless">
+        {" "}
+        <img src={homeless} alt="" className="start_page_homeless" />
+      </div>
+      <div className="start_page_start_button">
+        <StartButton name="GAME START" />
+      </div>
+      <div className="start_page_explain_button">
+        <ExplainButton name="遊び方" />
+      </div>
+      <img src={George} alt="" className="George" />
 
-      <NavLink to="/level">レベルだよ</NavLink>
       <NavLink to="/explain">説明</NavLink>
     </div>
   );
